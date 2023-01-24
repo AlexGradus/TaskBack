fde0f956b3e3793b7dacd458c7c8e9fab4a1c3d5
AlexGradus <countr@mail.ru>
2023-01-24 15:56:34 +0300

    add:back

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require("config");
 const authRouter = require("./routes/auth.routes");
 
 const app = express();
-const PORT=config.get('PORT');
+const PORT=process.env.PORT || 4000;
 const corsMiddleWare = require("./middleware/cors.middleware");
 app.use(corsMiddleWare);
 app.use(express.json());
